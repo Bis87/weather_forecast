1b7f40fd44bacbaccb63bcf19e3a968b401818f0
Bis87 <pavel.rytvinsky@gmail.com>
2017-01-17 23:10:54 +0300

    problem with ssl

--- kino_chooser.rb
+++ kino_chooser.rb
@@ -1,12 +1,20 @@
 require 'mechanize'
+require "openssl"
 
 agent = Mechanize.new()
 
-film_title = nil
-
 choosen = false
 
+
+# puts OpenSSL::OPENSSL_VERSION
+# puts "SSL_CERT_FILE: %s" % OpenSSL::X509::DEFAULT_CERT_FILE
+# puts "SSL_CERT_DIR: %s" % OpenSSL::X509::DEFAULT_CERT_DIR
+
+
+
+
 until choosen
+
   page = agent.get("http://www.kinopoisk.ru/top/lists/1/filtr/all/sort/order/perpage/100/page/#{rand(5)}/")
 
   tr_tag = page.search("//tr[starts-with(@id, 'tr_')]").to_a.sample
@@ -15,4 +23,16 @@ until choosen
   rating = tr_tag.search("span[@class = 'all']").text
   link = "https://www.kinopoisk.ru/film/#{tr_tag.attributes['id']}/"
   description = tr_tag.search("span[@class = 'gray_text']")[0].text
+
+  puts title
+  puts description
+  puts "kinopoisk rating:#{rating}"
+
+  puts "this one? (Y/N)"
+
+  choice = STDIN.gets.chomp
+
+  if choice.downcase == 'y'
+    choosen = true
+  end
 end
