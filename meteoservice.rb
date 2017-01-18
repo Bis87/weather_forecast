7f48a17a9e364ffe42b8dcef1cda0c68724252c3
Павел Рытвинский <p.rytvinsky@invento.by>
2017-01-18 13:02:31 +0300

    added forecast for tomorrow

--- meteoservice.rb
+++ meteoservice.rb
@@ -3,6 +3,7 @@
 require 'net/http'
 require 'uri'
 require 'rexml/document'
+require 'date'
 
 #словарик для cloudiness
 CLOUDINESS = {0 => 'clear', 1 => 'light cloudy', 2 => 'cloudy', 3 => 'Mainly cloudy'}
@@ -15,21 +16,48 @@ doc = REXML::Document.new(response.body)
 
 city_name = URI.unescape(doc.root.elements['REPORT/TOWN'].attributes['sname'])
 
-current_forecast = doc.root.elements['REPORT/TOWN'].elements.to_a[0]
 
-min_temp =  current_forecast.elements['TEMPERATURE'].attributes['min']
-max_temp =  current_forecast.elements['TEMPERATURE'].attributes['max']
+all_forecasts = doc.root.elements['REPORT/TOWN'].elements.to_a
 
-max_wind = current_forecast.elements['WIND'].attributes['max']
+i = 0
 
-cloud_index = current_forecast.elements['PHENOMENA'].attributes['cloudiness'].to_i
-clouds = CLOUDINESS[cloud_index]
+puts all_forecasts[0]
 
-puts city_name
+while i < all_forecasts.length
 
-puts "temperature: min: #{min_temp} max: #{max_temp}"
+  current_forecast = doc.root.elements['REPORT/TOWN'].elements.to_a[i]
 
-puts "wind: #{max_wind} m/s"
+  current_date = "#{current_forecast.attributes['day']}-#{current_forecast.attributes['month']}-#{current_forecast.attributes['year']}"
 
-puts "cloudiness: #{clouds}"
+  min_temp =  current_forecast.elements['TEMPERATURE'].attributes['min']
+  max_temp =  current_forecast.elements['TEMPERATURE'].attributes['max']
+
+  max_wind = current_forecast.elements['WIND'].attributes['max']
+
+  cloud_index = current_forecast.elements['PHENOMENA'].attributes['cloudiness'].to_i
+  clouds = CLOUDINESS[cloud_index]
+
+  if Date.parse(current_date).strftime('%d-%m-%Y') == Date.today.strftime('%d-%m-%Y')
+    puts "Today"
+  else
+    puts "date: #{current_date}"
+  end
+
+
+  puts "temperature: min: #{min_temp} max: #{max_temp}"
+
+  puts "wind: #{max_wind} m/s"
+
+  puts "cloudiness: #{clouds}"
+
+  i += 1
+
+end
+
+
+
+
+
+
+
 
